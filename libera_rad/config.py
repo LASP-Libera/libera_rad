b5e4edc654096b9ed606a7ffaf50e33a5810b56d
Matthew Maclay <matthew.maclay@lasp.colorado.edu>
2026-05-13 09:52:11 -0600

    Add solar calibration product definitions to config

--- libera_rad/config.py
+++ libera_rad/config.py
@@ -15,6 +15,18 @@ product_config_path = Path(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "L1B_RAD-4CH_product_definition.yml")
 )
 
+cal_solar_cal_product_definitions = {
+    DataProductIdentifier.cal_solar_cal_face1_combined: Path(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "solar_cal_face1_l1a.yml")
+    ),
+    DataProductIdentifier.cal_solar_cal_face2_combined: Path(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "solar_cal_face2_l1a.yml")
+    ),
+    DataProductIdentifier.cal_solar_cal_face3_combined: Path(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "solar_cal_face3_l1a.yml")
+    ),
+}
+
 cal_lw_cal_product_definitions = {
     DataProductIdentifier.cal_lw_cal_temp1_combined: Path(
         os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "CAL_LW_CAL_TEMP1_product_definition.yml")
